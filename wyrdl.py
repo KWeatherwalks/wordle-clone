5e9fec4da50afd55247053f682069b337072c321
KWeatherwalks <kevin.weatherwalks@gmail.com>
2023-02-08 10:56:47 -0700

    allow for up to 6 guesses

--- wyrdl.py
+++ wyrdl.py
@@ -1,7 +1,8 @@
 # wyrdl.py
 
-guess = input("Guess a word: ").upper()
-if guess == "SNAKE":
-    print("Correct")
-else:
+for guess_num in range(1, 7):
+    guess = input("Guess a word: ").upper()
+    if guess == "SNAKE":
+        print("Correct")
+        break
     print("Wrong")
